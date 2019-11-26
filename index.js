8eb0bd0fe0bb444cc16a96503bbecf69e425803f
ashya <ashyamanning@pursuit.org>
2019-11-25 21:53:35 -0500

    solve problem 9

---
+++ index.js
@@ -0,0 +1,24 @@
+// Part One
+
+// 1. A random cat fact
+// https://catfact.ninja/fact?max_length=140
+
+// 2. A list of 150 random users in English.
+// https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole
+
+// 3. All the repos on Github with Pursuit their name
+// 4. All the JavaScript repos on Github with Pursuit in their name
+// 5. All the Swift repos on Github with Pursuit in their name
+
+// 6. A list of all Pokemon
+// https://pokeapi.co/api/v2/growth-rate/2/
+
+// 7. A list of all items in Fortnite
+// https://www.fortnitehut.com/free-api?rarity=epic
+
+// 8. A list of all Game of Thrones Episodes.
+
+
+// 9. A list of all songs with "Love" in the title.
+// 10. All information about Petyr Baelish from the Game of Thrones books
+// 11. All the movies Leonardo Dicaprio has acted in
